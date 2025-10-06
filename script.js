c25f05e5bcb6cd5b82bf914bb24f992e8bb500e8
Neeraj Kumar Shukla <132157056+Neerajshukla12@users.noreply.github.com>
2025-10-06 22:35:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let squares = document.getElementsByClassName("square");
+
+for (let i = 0; i < squares.length; i++) {
+  squares[i].addEventListener("mouseenter", () => {
+    for (let j = 0; j < squares.length; j++) {
+      if (i !== j) {
+        squares[j].style.backgroundColor = "#6F4E37";
+      }
+    }
+  });
+  squares[i].addEventListener("mouseleave", () => {
+    for (let j = 0; j < squares.length; j++) {
+      squares[j].style.backgroundColor = "#E6E6FA";
+    }
+  });
+}
+
+
+
+
